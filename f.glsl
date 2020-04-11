50bb86ec8f0cf19762ced88907d33a6c2ca2cf1b
DurpBurger <twilight0999@gmail.com>
2020-04-11 13:30:04 -0500

    reflection code

--- f.glsl
+++ f.glsl
@@ -12,24 +12,36 @@ const float EPSILON = 0.0001f;
 const float ANTI_ACNE = 0.001f;
 const vec3 ZEROS = vec3(0, 0, 0);
 const float FAKE_FOV = 0.6;
+const int RAY_TREE_NODES = 3; // depth 1 recursion
 
 // --------------------- Function Headers
-bool trace(vec3 e, vec3 s, inout vec3 colour);
+bool trace(inout vec3 colour);
 void debugRed();
 void debugViewRect(float xPos, float yPos);
 bool getIntersection(vec3 e, vec3 d, inout float dist, out int indexOfClosest, out int indexOfTriangle);
 bool testIntersectionWithObject(int i, vec3 e, vec3 d, inout float dist, out int indexOfClosest, out int indexOfTriangle);
 bool checkIfInShadow(vec3 P, int lid, vec3 lightPos, out vec3 throughLight, int indexOfClosest, int indexOfTriangle);
 vec3 getShadowAmount(vec3 P, int lid, vec3 lightPos);
+bool getReflectionVector(inout vec3 d, vec3 N, vec3 V);
+bool getTransmissionVector(inout vec3 d );
 bool determineLightDirection(vec3 P, int lid, inout vec3 L, inout vec3 lightPos);
 vec3 phongIllumination(int oid, int lid, vec3 N, vec3 L, vec3 V);
 vec3 calcNormal(int oid, vec3 P, int indexOfTriangle);
 float calcPlaneDistance(vec3 A, vec3 N, vec3 d, vec3 e);
 float acneThreshold(vec3 N, vec3 d);
+void clamp(inout vec3 vec);
 
 // --------------------- Structs
 struct RayResult {
   vec3 colour;
+};
+
+struct RayResult2 {
+  vec3 e; // ray values
+  vec3 d;
+  vec3 colour; // result colour
+  int matid; // mat of hit spot
+  int prev;
 };
 
 // --------------------- Uniforms
@@ -50,6 +62,11 @@ float debugMax = 10.0; // red
 
 // --------------------- General Variables
 RayResult rays[RECURSION_LIMIT];
+RayResult2 myRays[RAY_TREE_NODES];
+bool isRay[RAY_TREE_NODES];
+
+vec3 background = vec3(1,1,1);
+
 int currRay = 0;
 int numRays = 1;
 
@@ -63,15 +80,32 @@ void main() {
     vec3 e = eyePos; //vec3(0, 0, 0);
     vec3 s = vec3(e.x + xPos * FAKE_FOV, e.y + yPos * FAKE_FOV, e.z - 1.0);
 
-    for (currRay = 0; currRay < RECURSION_LIMIT; currRay++) {
-		if (currRay >= numRays) { break; }
+	// for (currRay = 0; currRay < RAY_TREE_NODES; currRay++) {
+	// 	if (currRay >= numRays) { break; }
 
-        RayResult ray = rays[currRay];
-        //vec3 colour = vec3(1, 0, 0);
-        if (trace(e, s, ray.colour)) {
-            out_colour.rgb = ray.colour;
-        }
-    }
+    //     RayResult ray = rays[currRay];
+    //     //vec3 colour = vec3(1, 0, 0);
+    //     if (trace(e, s, ray.colour)) {
+    //         out_colour.rgb = ray.colour;
+    //     }
+    // }
+
+	for (currRay = 0; currRay < RAY_TREE_NODES; currRay++) {
+		myRays[currRay] = RayResult2(vec3(0,0,0), vec3(0,0,0), vec3(0,0,0), -1, -1);
+		isRay[currRay] = false;
+	}
+	//RayResult ray = rays[currRay];
+	myRays[0] = RayResult2(e, (s-e) ,vec3(0,0,0), -1, -1);
+	isRay[0] = true;
+
+	//vec3 finalColour = vec3(.5,.5,.5);
+	//vec3 colour = vec3(1, 0, 0);
+	if (trace(myRays[0].colour)) {
+		out_colour.rgb = myRays[0].colour;
+	} else {
+		out_colour.rgb = background;
+	}
+	//out_colour.rgb = myRays[0].colour;
 
     //rays[0].colour.z = geometry[2].z;
     //debug = rays[0].colour.z;
@@ -80,50 +114,143 @@ void main() {
     debugRed();
 }
 
+bool trace(inout vec3 colour) {
 
-bool trace(vec3 e, vec3 s, inout vec3 colour) {
-    float dist = FLT_MAX;
-    vec3 D = (s - e);
-    int indexOfClosest = -1;
-	int indexOfTriangle = -1;
+	int prev = -1;
+	for (currRay = 0; currRay < RAY_TREE_NODES; currRay++) {
+		if(!isRay[currRay]) {continue;} // only check if a ray is here
 
-    bool hit = getIntersection(e, D, dist, indexOfClosest, indexOfTriangle);
-    
-    if (hit) {
+		int reflectionRayIndex = currRay * 2 + 1;
+		int transmissionRayIndex = currRay * 2 + 2;
+
+		vec3 e = myRays[currRay].e;
+		vec3 D = myRays[currRay].d;
+
+		float dist = FLT_MAX;
+		int indexOfClosest = -1;
+		int indexOfTriangle = -1;
+
+		bool hit = getIntersection(e, D, dist, indexOfClosest, indexOfTriangle);
+		//isRay[currRay] = hit;
 		vec3 total = vec3(0, 0, 0);
-        int oid = objectIds[indexOfClosest];
 
-		vec3 P = e + (dist * D); // Point of intersection
-		vec3 N = calcNormal(oid, P, indexOfTriangle); // Normal at intersection point
-		vec3 V = normalize(e - P); // Vector from P to eye.
-		
-		for (int i = 0; i < NUM_LIGHTS; i++) { // For each light
-			if (i == numLights) { break; }
-			int lid = lightIds[i];
+		if (hit) { 
+			isRay[currRay] == true; // mark current ray as valid
+			int oid = objectIds[indexOfClosest];
 
-			vec3 L = vec3(0, 0, 0);
-			vec3 lightPos = vec3(0, 0, 0);
-			vec3 throughLight = vec3(1,1,1);
+			int matid = int(geometry[oid + 1].r);
+    		myRays[currRay].matid = matid; // record mat of hit
 
-			if (!determineLightDirection(P, lid, L, lightPos)) { continue; }
+			vec3 P = e + (dist * D); // Point of intersection
+			vec3 N = calcNormal(oid, P, indexOfTriangle); // Normal at intersection point
+			vec3 V = normalize(e - P); // Vector from P to eye.
+			
+			for (int i = 0; i < NUM_LIGHTS; i++) { // For each light
+				if (i == numLights) { break; }
+				int lid = lightIds[i];
 
-			vec3 light = getShadowAmount(P, lid, lightPos);
-			//if (checkIfInShadow(P, lid, lightPos, throughLight, indexOfClosest, indexOfTriangle)) { continue; }
-			total += phongIllumination(oid, lid, N, L, V) * light;
+				vec3 L = vec3(0, 0, 0);
+				vec3 lightPos = vec3(0, 0, 0);
+				vec3 throughLight = vec3(1,1,1);
+
+				if (!determineLightDirection(P, lid, L, lightPos)) { continue; }
+
+				vec3 light = getShadowAmount(P, lid, lightPos);
+				//if (checkIfInShadow(P, lid, lightPos, throughLight, indexOfClosest, indexOfTriangle)) { continue; }
+				if (reflectionRayIndex > RAY_TREE_NODES) { 
+					//materials[matid + 4] * 
+					total += phongIllumination(oid, lid, N, L, V) * light; // can't cast another reflection ray, so just scale now
+				} 
+				else {
+					total += phongIllumination(oid, lid, N, L, V) * light;
+				}
+				//clamp(total);
+				myRays[currRay].colour = total;
+			}
+			
+			vec3 reflectionMat = materials[matid + 3];
+			vec3 transmissionMat = materials[matid + 4];			
+			
+			if (reflectionRayIndex < RAY_TREE_NODES && length(reflectionMat) > 0.01)  {// some threshold
+				if (getReflectionVector(myRays[reflectionRayIndex].d, N, V)) {
+					myRays[reflectionRayIndex].e = P;
+					isRay[reflectionRayIndex] = true;
+					myRays[currRay].prev = prev;
+					prev = currRay;
+					//nextLargestRay = min(nextLargestRay, reflectionRayIndex);
+				}
+			}
+
+			if (transmissionRayIndex < RAY_TREE_NODES && length(transmissionMat) > 0.01)  {// some threshold
+				if (getTransmissionVector(myRays[transmissionRayIndex].d)) {
+					//isRay[transmissionRayIndex] = true;
+				}
+			}
+			
+			//total += calcReflection(object, P, N, V, outside, pick, recursionLevel);
+			//total = calcTransmission(object, P, V, total, outside, pick, recursionLevel);
+			//total = calcRefraction(object, P, N, V, total, outside, pick, recursionLevel);
+
+			//colour = total;
+			//return true;
+		} else {
+			//isRay[currRay] = false;
+			//myRays[currRay].colour = background;
+			//myRays[currRay].useBackground = true;
 		}
-
-		//total += calcReflection(object, P, N, V, outside, pick, recursionLevel);
-		//total = calcTransmission(object, P, V, total, outside, pick, recursionLevel);
-		//total = calcRefraction(object, P, N, V, total, outside, pick, recursionLevel);
-
-		colour = total;
-	}
     
     //debug = dist;
+	}
 
-    return hit;
+	int index = prev;
+	// traverse from leaves up
+	//while()
+	for (currRay = RAY_TREE_NODES - 1; currRay > 0; currRay--) {
+		//if(!isRay[currRay]) {continue;}
+		
+		//if(!isRay[currRay]) { continue;}
+		if(index == -1){
+			break;
+		}
+
+		int parentIndex;
+		int colourFactorIndex; // either the parent reflective/transmission value;
+
+		if (index % 2 == 0) { // this is transmission ray
+			// parentIndex = (currRay / 2);
+
+			// int parentMatId = myRays[parentIndex].matid;
+			// colourFactorIndex = parentMatId + 4;
+		} 
+		else { // reflection is reflection ray
+			parentIndex = index / 2;
+			int parentMatId = myRays[parentIndex].matid;
+			colourFactorIndex = parentMatId + 3;	
+		}	
+
+		vec3 parentColourFactor = materials[colourFactorIndex];
+		vec3 _parentColourFactor = vec3(1,1,1) - parentColourFactor;
+		myRays[parentIndex].colour = myRays[parentIndex].colour * _parentColourFactor + myRays[index].colour * parentColourFactor;
+
+		index = myRays[index].prev;
+	}
+
+
+	//myRays[0].colour = myRays[0].colour * vec3(.5,.5,.5) + myRays[1].colour * vec3(.5,.5,.5);
+	colour = myRays[0].colour; // initial ray colour
+
+    return true;//isRay[0];
 }
 
+//
+bool getReflectionVector(inout vec3 d, vec3 N, vec3 V) {
+	d = normalize(2.0 * dot(N, V) * N - V); // Reflection direction
+	return true;
+}
+
+bool getTransmissionVector(inout vec3 d ) {
+	return true;
+}
 
 // vec3 calcReflection(int oid, vec3 P, vec3 N, vec3 V, bool outside, bool pick) {
 // 	vec3 result = ZEROS;
@@ -146,6 +273,9 @@ bool trace(vec3 e, vec3 s, inout vec3 colour) {
 bool getIntersection(vec3 e, vec3 d, inout float dist, out int indexOfClosest, out int indexOfTriangle) {
 
 	for (int i = 0; i < NUM_OBJECTS; i++) {
+
+		if (i == numObjects)
+			break;
         testIntersectionWithObject(i, e, d, dist, indexOfClosest, indexOfTriangle);
 	} // for each object
 
@@ -372,6 +502,12 @@ float acneThreshold(vec3 N, vec3 d) {
 	}
 
 	return acneThreshold;
+}
+
+void clamp(inout vec3 vec) {
+	vec.r = max(0, min(1, vec.r));
+	vec.g = max(0, min(1, vec.g));
+	vec.b = max(0, min(1, vec.b));
 }
 
 
